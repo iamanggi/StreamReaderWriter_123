5ba5c394c3382051fb8be1c757f131ed299ba280
iamanggi <anggipuspita452@gmail.com>
2023-06-19 11:17:11 +0700

    membuat manual exception

--- ManualException/ManualException.cpp
+++ ManualException/ManualException.cpp
@@ -5,5 +5,17 @@ int main()
 {
 	try {
 		cout << "Selamat Belajar di Prodi TI Umy" << endl;
+		throw 1; // melemparkan sebuah integer maka
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
 	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		// jika selain integer maka block ini akan dieksekusi 
+		cout << "default pengecualian dieksekusi" << endl;
+	}
+
+	return 0;
 }
