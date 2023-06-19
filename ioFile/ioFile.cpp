8a968e737cccc03c97c0d117c38dc1afddcc4995
iamanggi <anggipuspita452@gmail.com>
2023-06-19 10:23:43 +0700

    Add project files.

---
+++ ioFile/ioFile.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main() {
+	string baris;
+
+	//membuka file dalam mode menulis.
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open("contohIofile.txt");
+}
